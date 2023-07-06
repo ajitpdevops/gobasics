2243ab719fe97c461aff959d53f2ada9b02d95d3
Ajit_Patil <ajitp.devops@outlook.com>
2023-07-06 08:35:00 +0530

    New folder added

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -6,8 +6,14 @@ import (
 	"net/http"
 )
 
+// TODO:
+// 1. Do not load template to disk every time
+// 2. Create a template cache
+// 3. Check if the data already exists in the templateCache
+// 4. Reload only if it does not exist and save the data in the templateCache
+
 func RenderTemplate(w http.ResponseWriter, tmpl string){
-	parsedTemplates, _ := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplates, _ := template.ParseFiles("./templates/" + tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplates.Execute(w, nil)
 	if err != nil {
 		log.Printf("Error parsing template %s", err)
